379e228913609016f0f1d5941dc587de93237851
Giovane Carrião <giovanecpds@outlook.com>
2026-05-11 09:14:12 -0300

    alterando numero máximo para 100

--- Projeto Base/app.js
+++ Projeto Base/app.js
@@ -1,7 +1,7 @@
 
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroMaximo = 5000;
+let numeroMaximo = 100;
 //Operador Math.random por si só gera um número decimal entre 0 e 1
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 
